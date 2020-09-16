1d6a6a4f541b2393731078b482a548de78e45b11
Raphael Augusto P <rafadastan02@live.com>
2020-09-15 22:04:49 -0300

    finalizando o teste automatizado

---
+++ Projeto.Xunit.BDD/TestUnit.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Projeto.Xunit.BDD
+{
+    public class TestUnit
+    {
+        [Fact]
+        public void Test()
+        {
+
+        }
+    }
+}
